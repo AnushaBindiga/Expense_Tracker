2320ea3030c918e5c735ad31038d53d1ead72ca3
Anusha Bindiga <anushabindiga@gmail.com>
2026-04-27 14:24:02 +0200

    Add vire entries option to display all entries

--- expense_tracker.py
+++ expense_tracker.py
@@ -24,18 +24,34 @@ def add_expense():
 
     print(Fore.GREEN + "\n Expense added successfully!!")
 
+def view_expenses():
+    print("\n--- All Expenses ---")
+    with open(EXPENSES_FILE, mode='r') as file:
+        reader = csv.DictReader(file)
+        expenses = list(reader)
+    if not expenses:
+        print(Fore.RED + "No expenses found!")
+        return
+    print(f"n{'No.':<5}{'Date':<15}{'Category':<15}{'Description':<25}{'Amount(€)':<10}")
+    print("-"*70)
+    for i, row in enumerate(expenses, 1):
+        print(f"{i:<5} {row['date']:<15} {row['category']:<15} {row['description']:<25} {row['amount']:<10}")
+
 def main():
     initialize_csv()
     while True:
         print("\n==== Expense Tracker ====")
         print("1. Add expense")
-        print("2. Exit")
+        print("2. View All expenses")
+        print("3. Exit")
         choice = input("\nChoose an option: ")
 
         if choice == "1":
             add_expense()
         elif choice == "2":
-            print("Goodbye!")
+            view_expenses()
+        elif choice == "3":
+            print("Goodbye!!")
             break
         else:
             print(Fore.RED + "Invalid option, try again.")
